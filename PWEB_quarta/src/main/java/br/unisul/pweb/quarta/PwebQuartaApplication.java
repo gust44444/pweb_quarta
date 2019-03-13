8418ff3f6bb248c1486c8c9d0e469b8c11b1494c
gust44444 <gustavoheidemann1@gmail.com>
2019-03-13 20:12:28 -0300

    Projeto criado

---
+++ PWEB_quarta/src/main/java/br/unisul/pweb/quarta/PwebQuartaApplication.java
@@ -0,0 +1,13 @@
+package br.unisul.pweb.quarta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwebQuartaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwebQuartaApplication.class, args);
+	}
+
+}
